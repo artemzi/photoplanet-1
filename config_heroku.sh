db311d45e54b3c71b1ee425641673188bfda06b4
Artem Zinoviev <arzinoviev@gmail.com>
2013-05-27 12:53:47 +0300

    опять забыл убрать лишнее...

--- config_heroku.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-heroku config:set DJANGO_SETTINGS_MODULE=photoplanet.settings.heroku
-heroku config:set VK_APP_ID='3648592'
-heroku config:set VK_API_SECRET='9Qeyol8q31xDqx47BWus'
---
+++ config_heroku_example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+heroku config:set DJANGO_SETTINGS_MODULE=photoplanet.settings.heroku
+heroku config:set VK_APP_ID=''
+heroku config:set VK_API_SECRET=''
